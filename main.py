8080ab7e4a64017172fce2fa62a73b19934d9342
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-10-21 21:27:56 -0300

    up

--- main.py
+++ main.py
@@ -24,11 +24,27 @@ def mostrar_menu():
 def tarefa_adicionar(tarefas):
     tarefa = input("Digite a tarefa que deseja adicionar: ")
     tarefas.append(tarefa)
+    id.append(id)
     print(f'Tarefa "{tarefa}" adicionada com sucesso!')
 
 def remover_tarefa(tarefas):
-    if  not  tarefas:
-        print("Não há tarefas para remover.")
-        return
+    remover = input ("Qual tarefa deseja remover?")
+    if remover in tarefas:
+        tarefas.remove(remover)
+        print(f'Tarefa "{remover}" removida com sucesso!')
     else:
-        remover = inpur ("Qual tarefa quer remover?")
+        print(f'Tarefa "{remover}" não encontrada.')
+
+def ver_tarefas(tarefas):
+    if not tarefas:
+        print("Nenhuma tarefa encontrada.")
+    else:
+        ver = input("Qual tarefa você deseja ver?")
+        if ver in tarefas:
+            print
+            
+        else:
+            
+            
+            
+        
