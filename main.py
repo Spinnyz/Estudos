2171e8f155ceac97cf05468ff99927433834e2c2
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-06-16 22:28:46 -0300

    up

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def soma(x,y):
-    return (x+y)
+def soma(*args):
+    total = 0
+    for i in args:
+        total +=i
+        print ("total:", i,total)
+    return total
+
+
+valor = soma(1,2,3,4,5,6,7,8,9,10,11,12)
+print(valor)
+    
 
