b948a74865c2429d533ac9dcdb5d36668767d4cd
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-06-28 20:49:52 -0300

    refactor: remove function definitions and replace with tuple unpacking and dictionary declarations

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-def executa (funcao, *args):
-    return funcao(*args)
-
-def soma (x,y):
-        return x+y
-    
-def criar_mult(multiplicador):
-        def multiplica(valor):
-            return valor*multiplicador
-    
-        return multiplica
+a,b = 1,2
+a,b = b,a
 
 
-print (
-    executa (
-        lambda multiplicador,valor : valor*multiplicador, 3,2
-    ),
-    executa (criar_mult(3),2)
-)
+pessoa = {
+    "nome": "Pedro",
+    "sobrenome" : "Lucas",
+
+}
+
+
+dados = {
+    "idade": 16,
+    "altura": 1.6,
+}
+
